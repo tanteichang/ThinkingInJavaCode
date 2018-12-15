f6ef0edbd850f9b6ca4a91aac7796419e65879a9
tantei <tanteichang@outlook.com>
2018-12-15 22:43:57 +0800

    init project

---
+++ src/com/tantei/c01/HelloDate.java
@@ -0,0 +1,10 @@
+package com.tantei.c01;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello. it is: ");
+        System.out.println(new Date());
+    }
+}
